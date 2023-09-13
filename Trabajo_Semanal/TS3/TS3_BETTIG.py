53868910e08982e23e088a19a60e7e05ee2ced61
Santiago Bettig <santiagobettig@frba.utn.edu.ar>
2023-09-12 21:31:56 -0300

    Se agrega el notebook correspondiente a la TS3 y se hacen algunas modificaciones sobre el script de python

--- Trabajo_Semanal/TS3/TS3_BETTIG.py
+++ Trabajo_Semanal/TS3/TS3_BETTIG.py
@@ -18,6 +18,7 @@ una distribución uniforme y luego se lo muestreara y cuantificará
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import scipy.signal as sig
 
 def sin_gen(vmax = 1, dc = 0, ff = 1, ph = 0, nn = 1000, fs = 1000):
     # Lo primero que hay que hacer es crear un vector temporal que determinará
@@ -57,10 +58,10 @@ def noise_gen(snr, N):
 # Defino los datos del ADC:
 
 Vr = 2;                         # Tensión de referencia del ADC
-n_bits = 16;                     # Cantidad de bits del ADC
+n_bits = 12;                     # Cantidad de bits del ADC
 quantum = Vr / (2**(n_bits-1)); # Calculo el valor del cuanto (Cada paso del ADC)
 
-# Con esta frecuencia representaré la señal sobremuestreada 
+# Con esta frecuencia representaré la señal analógica 
 
 f_cont  = 500       # Frecuencia de muestreo
 N       = f_cont    # Cantidad de muestras
@@ -70,30 +71,31 @@ N       = f_cont    # Cantidad de muestras
 
 f_sin   = 10; # Frecuencia de mi senoidal
 snr     = 30; # Relación señal a ruido
-A_sin   = 1;  # AMplitud de Senoidal
+A_sin   = 1;  # Amplitud de Senoidal
+
+t, y_sin = sin_gen(A_sin, 0, f_sin, 0, N, f_cont)
+y_noise = (A_sin/(math.sqrt(2))) * noise_gen(snr, N);
+noisy_sin = y_sin + y_noise;
 
 fig1, (ax1, ax2, ax3) = plt.subplots(3);
 
 # Lo primero que voy a hacer es generarme la señal senoidal 
 # sin_gen(Amp, DC, Frec, Phase, Muestras, Fs)
-t, y_sin = sin_gen(A_sin, 0, f_sin, 0, N, f_cont)
 ax1.plot(y_sin, color = 'green', label = 'Función Senoidal')
 ax1.legend(loc = 'upper right');
 
 # Luego voy a generar la señal aleatoria, donde el primero parámetro
 # será la relación señal a ruido deseada y el segundo parámetro la 
 # cantidad de muestras
-y_noise = (A_sin/(math.sqrt(2))) * noise_gen(snr, N);
 ax2.plot(y_noise, color = 'blue', label = 'Función Aleatoria')
 ax2.legend(loc = 'upper right');
 
 # Por último, voy a desnormalizar y realizar la suma de ambas
-noisy_sin = y_sin + y_noise;
 ax3.plot(noisy_sin, color = 'red', label = 'Senoidal Ruidosa')
 ax3.legend(loc = 'upper right');
 
 # Lo que voy a hacer ahora, para simular un muestreador, es quedarme con
-# una cantidad menor de muestras, 1 cada ovs_rate muestras.
+# una muestra cada "ovs_rate" muestras.
 ovs_rate = 4;
 noisy_sin_sampled = noisy_sin[::ovs_rate]
 # Luego voy a realizar la cuantificación, para ello voy a generar el redondeo
@@ -114,18 +116,43 @@ ax1.legend(loc = 'upper right');
 ax2.plot(quantif_noise/quantum, color = 'red', label = 'Ruido de Cuantificación')
 ax2.legend(loc = 'upper right');
 
-fig3, (ax1, ax2) = plt.subplots(2);
+# Obtengo el valor medio del ruido de cuantificación, el cual debería 
+# ser cero
+mean_quantif_noise = np.mean(quantif_noise);
 
-ax1.plot(quantif_noise/quantum, color = 'red', label = 'Ruido de Cuantificación')
+# Tengo tres formas distintas de obtener la varianza del ruido: La primera
+# de ellas se obtiene de suponer una distribución uniforme del mismo, la 
+# segunda es a través de la herramienta de cálculo que proporciona numpy,
+# y la última, se obtiene de la autocorrelación de la señal. Esta última se
+# cumplirá unicamente si el ruido está totalmente incorrelacionado, obteniendo
+# un delta de kronecker en el origen (representando la energía de la señal)
+var_quantif_noise_1 = (np.float64(quantum)**2)/12
+var_quantif_noise_2 = np.var(quantif_noise)
+var_quantif_noise_3 = sig.correlate(quantif_noise, quantif_noise)/(N/ovs_rate)
+
+fig3, (ax1, ax2, ax3) = plt.subplots(3);
+
+# Voy a imprimir el ruido de cuantización normalizado en quantum, esto me 
+# fijará el límite superior en quantum/2 y el inferior en -quantum/2
+ax1.stem(quantif_noise/quantum, label = 'Ruido de Cuantificación')
 ax1.legend(loc = 'upper right');
 
-ax2.hist(quantif_noise/quantum, color = 'blue', label = 'Distribución');
+# Por otro lado, voy a imprimir la distribución, que se todo salió acorde 
+# a la teoría, debería ser uniforme
+ax2.hist(quantif_noise, color = 'green', label = 'Distribución');
 ax2.legend(loc = 'upper right');
 
-var_quantif_noise = np.var(quantif_noise);
-var_quantif_noise_v2 = (quantum**2)/12;
-correlate_quantif_noise = np.correlate(quantif_noise, quantif_noise)/N;
-mean_quantig_noise = np.mean(quantif_noise);
+# Por último, voy a imprimir los resultados de la autocorrelación de la señal
+# Si todo es correcto, debería haber una delta en el origen cuya amplitud
+# es igual a la varianza de la señal
+ax3.plot(var_quantif_noise_3, color = 'red', label = 'Correlación');
+ax3.legend(loc = 'upper right');
+
+print("El valor medio es:",mean_quantif_noise,"el cual debería ser cercano a cero")
+print("La varianza obtenida de suponer una distribución uniforme es:", var_quantif_noise_1)
+print("La varianza obtenida a partir de la expresión de numpyy es:", var_quantif_noise_2)
+print("La varianza obtenida a partir de suponer un ruido totalmente incorrelacionado es", max(var_quantif_noise_3))
+
 
 
 
